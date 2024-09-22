66fdb31e075f494876b68fce0f62a92a813e1ba9
Makkohli <manish.kohli.ug21@nsut.ac.in>
2024-09-22 14:19:00 +0530

    added company endpoints

--- server_side/routes/index.js
+++ server_side/routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import authRouter from './auth.js';
 import noticeRouter from './notice.js'; // New route for notices
+import editprofRouter from './editProf.js';
+import companyDetails from './companydetails.js';
 
 const router = express.Router({ mergeParams: true });
 
@@ -11,5 +13,7 @@ router.get("/", (req, res) => {
 router.use("/auth", authRouter);
 
 router.use("/notices", noticeRouter); // Add notice routes
+router.use("/editProf",editprofRouter);
+router.use('/company', companyDetails);
 
 export default router;
