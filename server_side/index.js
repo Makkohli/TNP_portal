d4c07a1bb3e6163078a1d69ae8762e91813ff694
Manish Kohli <48186851+Makkohli@users.noreply.github.com>
2024-10-10 14:44:48 +0530

    Update index.js

--- server_side/index.js
+++ server_side/index.js
@@ -7,38 +7,30 @@ import rootRouter from "./routes/index.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 // MongoDB connection
 async function connectDB() {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("DB connected");
-    } catch (error) {
-        console.error("DB connection error:", error);
-    }
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("DB connected");
+  } catch (error) {
+    console.error("DB connection error:", error);
+  }
 }
+app.options('*', cors());  
 
-// CORS configuration
-const allowedOrigins = [
-    'https://tnp-portal-lake.vercel.app',  // Frontend URL
-    'http://localhost:3001'  // Local development URL
-];
-
-app.use(cors({
-    origin: allowedOrigins,
-    credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
-    optionsSuccessStatus: 200,  // For legacy browser support
-}));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.options('*', cors());  // Handle preflight requests for all routes
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 app.use("/api/v1", rootRouter); // Root router
 
 // Start server
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
