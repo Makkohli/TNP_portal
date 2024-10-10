551c9e712d551340357b30bd18b1fba90855ebcc
Manish Kohli <48186851+Makkohli@users.noreply.github.com>
2024-10-10 13:23:13 +0530

    Update index.js

--- server_side/index.js
+++ server_side/index.js
@@ -4,22 +4,12 @@ import cors from "cors";
 import dotenv from 'dotenv';
 import rootRouter from "./routes/index.js";
 
-
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-
-
-app.use(cors({
-   origin: '*',  // Allow requests from all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-
-}));
+app.use(cors({ origin: '*' }));
 
 // MongoDB connection
 async function connectDB() {
@@ -30,10 +20,6 @@ async function connectDB() {
     console.error("DB connection error:", error);
   }
 }
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
 
 app.use("/api/v1", rootRouter); // Root router
 
